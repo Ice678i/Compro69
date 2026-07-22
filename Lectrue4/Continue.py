6903057338ac711ef41053d64ce4eed3ae6182be
Ice678i <baolo199789@gmail.com>
2026-07-22 04:15:16 +0000

    save

---
+++ Lectrue4/Continue.py
@@ -0,0 +1,5 @@
+for letter in 'Thanawat Baolo':
+    if letter == 'a' or letter == 'o':
+        continue
+    print('Current Letter :', letter)
+    
--- Lectrue4/Exercise_III.py
+++ Lectrue4/Exercise_III.py
@@ -1,5 +1,8 @@
-print("1-100")
+column = int(input("Enter the number of columns: "))
 
 for i in range(1, 101):
-    print(i, end="  ")
+    print(i, end="\t")
+
+    if i % column == 0:
+        print()
     
