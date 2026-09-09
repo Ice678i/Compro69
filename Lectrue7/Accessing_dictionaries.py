c14cb094570e269db508214ecb5f0e6c8b889b79
Ice678i <baolo199789@gmail.com>
2026-09-09 11:06:21 +0700

    save L7

---
+++ Lectrue7/Accessing_dictionaries.py
@@ -0,0 +1,6 @@
+student = {"name": "Alice", "age": 25, "grade": "A", "major": "Computer Science"}
+
+for key in student:
+    print(f"{key}: {student[key]}")
+
+    
---
+++ Lectrue7/Set_operation-I.py
@@ -0,0 +1,13 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+union_set = set1 | set2
+print("Union:", union_set)
+
+intersection_set = set1 & set2
+print("Intersection:", intersection_set)
+
+defference_set = set1 - set2
+print("Defference:", defference_set)
+
+sym_diff_set = set1 ^ set2
+print("Symmetric Differance:", sym_diff_set )
